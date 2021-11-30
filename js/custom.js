71c76f9d3481f51879f218e0f9f588468ef6c92c
tnwjd2050 <tnwjd2050@naver.com>
2021-11-30 23:44:44 +0900

    포트폴리오 파일 추가

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+﻿const init = () => {
+	const $item = $("article.item");
+	const winwWidth = $(window).width();
+	const numAc = $item.size();
+	const articleWidth = $item.outerWidth(true);
+	const widSec = articleWidth * numAc;
+	const widTotal = widSec+600;
+	const $section = $("section");
+	const initLeft = $section.offset().left;
+
+	const setSize = () => {
+		$section.width(widTotal);
+		$("body").height(widSec);	
+	};
+
+	const moveScroll = (_this) => {
+		const scroll = $(_this).scrollTop();
+		const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+		const amount = scrollBottom === 0 ? winwWidth - initLeft - scroll : initLeft - scroll;
+
+		$section.stop().animate({"left": amount},600);
+	}
+
+	setSize();
+	
+	$(window).on("scroll", () => {
+		moveScroll(this);
+	});
+}
+
+$(document).ready(init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
