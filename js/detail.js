a2ada7254979762c0a280aadbb0d0f9ff87b6179
tnwjd2050 <tnwjd2050@naver.com>
2021-12-03 00:23:56 +0900

    상세 - 화면 추가

--- js/detail.js
+++ js/detail.js
@@ -1,17 +1,54 @@
-const main = ((d, w, $) => {
+const main = (function(d, w, $)  {
 	let init;
 	init = init || {};
 
-	const ui  = () => {
-		const loadHeader = () => {
+	const ui  = function() {
+		const loadHeader = function() {
 			$('header').load("../header.html");
 		};
 
 		loadHeader();
 	}
 
-	init = () => {
+    const data = function() {
+        const getParam = function(paramName) {
+            let params = location.search.substr(location.search.indexOf("?") + 1);
+            let paramValue = "";
+
+            params = params.split("&");
+
+            for (let i = 0; i < params.length; i++) {
+                temp = params[i].split("=");
+                if ([temp[0]] == paramName) { paramValue = temp[1]; }
+            }
+
+            return paramValue;
+        };
+
+        const setHTML = function(objData) {
+            $('.item_thumb').html(`<img src="img/${objData.image}.jpg" alt="">`);
+            $('.title_client').text(`${objData.client}`);
+            $('.title_name').text(`${objData.name}`);
+            $('.contribution').text(`${objData.work.contribution}`);
+            $('.period').text(`${objData.work.period}`);
+            $('.detail').html(`${objData.info}`);
+            objData.tag.forEach(function(item) {
+                $('.tag').append(`<li>${item}</li>`)
+            })
+        };
+
+        const getJSONDetail = function(data) {
+            setHTML(data[getParam('index')])
+        };
+
+        // 로컬용
+        const jsonSrc = '../json/data.json';
+        $.getJSON(jsonSrc, getJSONDetail);
+    }
+
+	init = function() {
 		ui();
+        data();
 	}
 
 	return init;
