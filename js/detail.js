07a974f309b3b249724863e1f2d45ce99f511dc4
tnwjd2050 <tnwjd2050@naver.com>
2021-12-06 22:01:35 +0900

    상세 - 컨텐츠 추가

--- js/detail.js
+++ js/detail.js
@@ -40,6 +40,31 @@ const main = (function(d, w, $)  {
                 };
 
                 const HTML = function(obj) {
+                    const onlyMobile = function(data) {
+                        if (data.onlyMo !== undefined && data.onlyMo === true) {
+                            return '(모바일만 접근 가능)';
+                        }
+
+                        return '';
+                    };
+
+                    const showLink = function(data) {
+                        if (data.link !== undefined && data.link !== '') {
+                            return '<li><strong>링크' + onlyMobile(data) + '</strong> - <a href="'+ data.link + '" target="_blank">바로가기</a></li>';
+                        }
+
+                        return '';
+                    };
+
+                    const showRole = function(data) {
+                        console.log(data.work.role)
+                        if (data.work.role !== undefined && data.work.role !== '') {
+                            return '<li><strong>역할 - </strong>' + data.work.role + '</li>';
+                        }
+
+                        return '';
+                    };
+
                     return '<div class="slideItem">' +
                             '<div class="imageWrap">' +
                                     '<div class="item_thumb"><img src="img/item_' + obj.image + '.jpg" alt=""></div>' +
@@ -50,15 +75,14 @@ const main = (function(d, w, $)  {
                                         '<h1 class="title_name">' + obj.name + '</h1>' +
                                     '</div>' +
                                     '<ul class="workData">' +
-                                        '<li>' +
-                                            '<strong>링크</strong> - <a href="#">바로가기</a>' +
-                                        '</li>' +
+                                        showLink(obj) +
                                         '<li>' +
                                             '<strong>기여도</strong> - <span class="contribution">' + obj.work.contribution + '</span>%' +
                                         '</li>' +
                                         '<li>' +
                                             '<strong>작업기간</strong> - <span class="period">' + obj.work.period + '</span>' +
                                         '</li>' +
+                                        showRole(obj) +
                                     '</ul>' +
                                     '<p class="detail">' + obj.info + '</p>' +
                                     '<ul class="tag">' + tagHTML(obj.tag) + '</ul>' +
